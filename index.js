4f0a2e8135fd17fcd246197e8d26f046ed9d45cc
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-01-10 14:42:27 +0100

    return course-id

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
 
+courses = [
+    {id: 1, name: 'Course 1', author: 'James Miller'},
+    {id: 2, name: 'Course 2', author: 'Afolabi Opakunle'},
+    {id: 3, name: 'Course 3', author: 'Tunde Ednut'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send('[1, 2, 3, 4, 5, 6, 7]')
+    res.send(courses)
 })
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
+app.get('/api/courses/:id', (req, res) => {
+  const course =  courses.find(course => {
+     return course.id === +req.params.id
+  } );
+  if(!course) res.status(404).send(`Data id ${req.params.id} Not found`);
+  res.send(course)
 })
 
 const port = process.env.PORT || 3000
