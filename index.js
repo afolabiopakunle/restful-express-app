49a3081c813ba1f3c1796a0a80850c6378a67e4c
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-01-10 12:01:40 +0100

    courses endpoint works

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 })
 
+app.get('/api/courses', (req, res) => {
+    res.send('[1, 2, 3, 4, 5, 6]')
+})
 
 app.listen(3000, () => {
     console.log('app listening on port 3000...')
